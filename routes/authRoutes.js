2de19599d12eebada1447a79ec29301ba0172b86
toukir048 <anjumtanvir117@gmail.com>
2026-06-26 03:58:48 +0600

    Refactor auth routes to include additional user authentication endpoints

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,19 @@
 const express = require("express");
-const { registerUser } = require("../controllers/authController");
+const {
+  registerUser,
+  loginUser,
+  googleLogin,
+  getCurrentUser,
+  logoutUser,
+} = require("../controllers/authController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/google-login", googleLogin);
+router.get("/me", authMiddleware, getCurrentUser);
+router.post("/logout", logoutUser);
 
 module.exports = router;
